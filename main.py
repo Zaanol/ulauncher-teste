1fba72b55ceafdef0488e280c48ae6094df5f3e3
zanol <willian.zanol@totvs.com.br>
2023-09-13 14:24:02 -0300

    Init

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import uuid
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+
+logger = logging.getLogger(__name__)
+
+
+class TesteExtension(Extension):
+
+    def __init__(self):
+        logger.info('init UUID extension')
+        super(TesteExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name="Teste",
+                                         description="Teste 2",
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction("Teste 3")
+                                         ))
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    TesteExtension().run()
